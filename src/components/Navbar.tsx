328340c5762d1489ec57c025b170515b6d990d53
Nebo Milic <nebo.milic@gmail.com>
2023-03-24 12:40:23 +0100

    Fix tailwind configuration, add Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,39 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { styles } from '../styles'
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
 
 const Navbar: FC = () => {
-    return <div>Navbar</div>
+    const [active, setActive] = useState('')
+    return (
+        <nav
+            className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+        >
+            <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+                <Link
+                    className="flex items-center gap-2"
+                    onClick={() => {
+                        setActive('')
+                        window.scrollTo(0, 0)
+                    }}
+                    to="/"
+                >
+                    <img
+                        src={logo}
+                        alt="logo"
+                        className="w-9 h-9 object-contain"
+                    />
+                    <p className="text-white text-[18px] font-bold cursor-pointer">
+                        Adrian{' '}
+                        <span className="sm:block hidden">
+                            | Javascript Mastery
+                        </span>
+                    </p>
+                </Link>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
